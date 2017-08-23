5fda179cc20686c29e5e3a31f409f1301bd8b57a
kepekzul <kevinyuseti@gmail.com>
2017-08-23 20:25:59 +0700

    bikin home page user

--- MyFreeShare/Controllers/AuthenticationController.cs
+++ MyFreeShare/Controllers/AuthenticationController.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyFreeShare.Models;
 
 namespace MyFreeShare.Controllers
 {
     public class AuthenticationController : Controller
     {
-        // GET: Authentication
+        [HttpGet]
         public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(pengguna data)
+        {
+            using(var db = new MainDBContext())
+            {
+                if (db.dataPengguna.Where(x => x.username == data.username).Any())
+                {
+                    var dataUser = db.dataPengguna.First(x => x.username == data.username);
+                    string passwordDB = dataUser.password;
+                    if (data.password == passwordDB)
+                    {
+                        Session["username"] = dataUser.nama;
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        TempData["pesan"] = "password salah";
+                    }
+                }
+                else
+                {
+                    TempData["pesan"] = "username tidak terdaftar";
+                }
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult SignUp()
         {
             return View();
         }
---
+++ MyFreeShare/MainDBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyFreeShare.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace MyFreeShare
+{
+    public class MainDBContext:DbContext
+    {
+        public MainDBContext():base("DatabaseEntities")
+        {}
+        public DbSet<pengguna> dataPengguna { get; set; }
+        public DbSet<File> dataFile { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
